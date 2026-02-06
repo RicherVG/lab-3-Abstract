9f68620ac9d8280165c235c68929b99ace8cfc82
RicherVG <166271618+RicherVG@users.noreply.github.com>
2026-02-06 08:38:41 -0600

    subiendo proyecto inicial

---
+++ lab3Abstract/src/lab3abstract/Lab3Abstract.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab3abstract;
+
+/**
+ *
+ * @author riche
+ */
+public class Lab3Abstract {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
