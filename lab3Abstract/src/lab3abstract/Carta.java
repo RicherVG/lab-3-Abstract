2ea85c7ee29dec6ae772da4ff18439025bbd782c
RicherVG <166271618+RicherVG@users.noreply.github.com>
2026-02-06 09:27:53 -0600

    correcion

--- lab3Abstract/src/lab3abstract/Carta.java
+++ lab3Abstract/src/lab3abstract/Carta.java
@@ -8,6 +8,36 @@ package lab3abstract;
  *
  * @author riche
  */
-public class Carta {
+public abstract class Carta {
+    protected String imagen;
+    protected boolean descubierta;
+    protected int id;
     
+    public Carta(String imagen,int id){
+        this.imagen=imagen;
+        this.descubierta=false;
+        this.id=id;
+    }
+    
+    public abstract void mostrar();
+    public abstract void ocultar();
+    public boolean estaDescubierta(){
+        return descubierta;
+    }
+    
+    public void setDescubierta(boolean descubierta){
+        this.descubierta=descubierta;
+    }
+    
+    public String getImagen(){
+        return imagen;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public boolean esParejaCon(Carta otraCarta){
+        return this.id== otraCarta.getId();
+    }
 }
--- lab3Abstract/src/lab3abstract/CartaPokemon.java
+++ lab3Abstract/src/lab3abstract/CartaPokemon.java
@@ -4,10 +4,51 @@
  */
 package lab3abstract;
 
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author riche
  */
-public class CartaPokemon {
+public class CartaPokemon extends Carta {
+    private JButton boton;
+    private static final String IMAGEN_DORSO="resources/dorso.png";
+
+    public CartaPokemon(String imagen, int id) {
+        super(imagen, id);
+        this.boton = new JButton();
+        configurarBoton();
+    }
+    
+    private void configurarBoton(){
+        boton.setIcon(new ImageIcon(IMAGEN_DORSO));
+        boton.setFocusPainted(false);
+        boton.setBorderPainted(false);
+        boton.setContentAreaFilled(false);
+    }
+    
+    @Override
+    public void mostrar() {
+         try {
+            boton.setIcon(new ImageIcon(imagen));
+            descubierta = true;
+        } catch (Exception e) {
+            System.err.println("Error al cargar imagen: " + imagen);
+        }
+    }
+
+    @Override
+    public void ocultar() {
+       boton.setIcon(new ImageIcon(IMAGEN_DORSO));
+       descubierta=false;
+    }
+    public JButton getBoton(){
+        return boton;
+    }
+    
+    public void desactivar(){
+        boton.setEnabled(false);
+    }
     
 }
